04ef42d41e3b7ef9a2de8fb6d6c94075eeceeda1
ayx99 <amanyadavx99@gmail.com>
2023-02-06 00:56:56 +0530

    adding script to install basic flatpak applications

---
+++ flatpak.sh
@@ -0,0 +1,12 @@
+#Flatpak must be installed and flathub should be added
+#vlc
+flatpak install -y org.videolan.VLC
+
+#Telegram Desktop
+flatpak install -y org.telegram.desktop
+
+#Gnome-extension-manager
+flatpak install -y com.mattjakeman.ExtensionManager
+
+#Brave
+flatpak install -y com.brave.Browser 
