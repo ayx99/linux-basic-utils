2c8837804616d7c506985c6a5b66d31687f5301f
Aman Yadav <amanyadavx99@gmail.com>
2023-02-08 00:05:21 +0530

    adding some more useful apps in flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -2,6 +2,9 @@
 #vlc
 flatpak install -y org.videolan.VLC
 
+#Extension Manager
+flatpak install -y com.mattjakeman.ExtensionManager 
+
 #Telegram Desktop
 flatpak install -y org.telegram.desktop
 
@@ -10,3 +13,7 @@ flatpak install -y com.mattjakeman.ExtensionManager
 
 #Brave
 flatpak install -y com.brave.Browser 
+
+#LibreWolf
+flatpak install -y io.gitlab.librewolf-community
+
